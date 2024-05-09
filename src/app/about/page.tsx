177cbddc531d704fefcd3a499a4e41eb913e32d4
SAGARCODE <sagar.jayale@gmail.com>
2024-05-09 13:44:54 +0300

    fixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,15 @@
-export default function page() {
+import { cookies } from "next/headers";
+import { serverSideFunction } from "@/utils/server-utils";
+
+export default function AboutPage() {
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
+  const result = serverSideFunction();
   return (
-    <div>
-      dsds
-    </div>
-  )
+    <>
+      <h1>About page {new Date().toLocaleTimeString()}</h1>
+      <p>{result}</p>
+    </>
+  );
 }
